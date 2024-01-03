189b7bc1c5517d29783965a5a8bf0b3f5531a3a8
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-03 17:29:34 +0900

    feat: 글수정용 dto 생성

---
+++ src/main/java/com/rowoon/myblog/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package com.rowoon.myblog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String Content;
+}
