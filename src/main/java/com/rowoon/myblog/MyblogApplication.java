d688f1eae25c0d337efa0b6451640b679913aecd
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-03 12:11:53 +0900

    환경설정 완료

---
+++ src/main/java/com/rowoon/myblog/MyblogApplication.java
@@ -0,0 +1,13 @@
+package com.rowoon.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogApplication.class, args);
+	}
+
+}
