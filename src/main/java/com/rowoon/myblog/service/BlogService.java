29d180558dd6bedd5e0da978dc68f283da196fa7
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-03 14:32:43 +0900

    feat: 글 추가 서비스 코드 생성

---
+++ src/main/java/com/rowoon/myblog/service/BlogService.java
@@ -0,0 +1,17 @@
+package com.rowoon.myblog.service;
+
+import com.rowoon.myblog.domain.Article;
+import com.rowoon.myblog.dto.AddArticleRequest;
+import com.rowoon.myblog.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
