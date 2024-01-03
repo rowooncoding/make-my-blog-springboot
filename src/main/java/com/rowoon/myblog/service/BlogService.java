784e678c4ea159e668e8891521fc286a022d83a6
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-03 15:53:19 +0900

    mod: service 어노테이션 추가 재 빌드

--- src/main/java/com/rowoon/myblog/service/BlogService.java
+++ src/main/java/com/rowoon/myblog/service/BlogService.java
@@ -4,8 +4,10 @@ import com.rowoon.myblog.domain.Article;
 import com.rowoon.myblog.dto.AddArticleRequest;
 import com.rowoon.myblog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service
 public class BlogService {
 
     private final BlogRepository blogRepository;
