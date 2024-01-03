39014e5f72504648a14252f1ed90b46023be1a72
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-03 14:21:44 +0900

    made: 엔티티 생성완료

--- src/main/java/com/rowoon/myblog/domain/Article.java
+++ src/main/java/com/rowoon/myblog/domain/Article.java
@@ -1,8 +1,42 @@
 package com.rowoon.myblog.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
 
 @Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    protected Article() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
