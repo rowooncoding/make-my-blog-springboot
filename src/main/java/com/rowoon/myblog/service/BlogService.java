2ede8a2f7e5ce84fd8291236e970a7877a44e635
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-03 16:33:32 +0900

    feat: 블로그 글 조회 서비스 코드 완료

--- src/main/java/com/rowoon/myblog/service/BlogService.java
+++ src/main/java/com/rowoon/myblog/service/BlogService.java
@@ -6,6 +6,8 @@ import com.rowoon.myblog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service
 public class BlogService {
@@ -15,5 +17,10 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 모든 글 조회
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
