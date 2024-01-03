4963673ca0e6f4d5549641e43427c3d90905b055
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-03 14:24:01 +0900

    made: 게터, 기본생성자 롬복으로 추가

--- src/main/java/com/rowoon/myblog/domain/Article.java
+++ src/main/java/com/rowoon/myblog/domain/Article.java
@@ -1,10 +1,14 @@
 package com.rowoon.myblog.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
 
     @Id
@@ -22,21 +26,5 @@ public class Article {
     public Article(String title, String content) {
         this.title = title;
         this.content = content;
-    }
-
-    protected Article() {
-
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
