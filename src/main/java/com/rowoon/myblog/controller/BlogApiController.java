3b377c1a89c54805b2eb91c9821b2a212dc68da9
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-03 16:48:56 +0900

    feat: 글들을 조회한 결과를 클라로 보내주는 컨트롤러 생성

--- src/main/java/com/rowoon/myblog/controller/BlogApiController.java
+++ src/main/java/com/rowoon/myblog/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.rowoon.myblog.controller;
 
 import com.rowoon.myblog.domain.Article;
 import com.rowoon.myblog.dto.AddArticleRequest;
+import com.rowoon.myblog.dto.ArticleResponse;
 import com.rowoon.myblog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 변환하는 컨트롤러
@@ -16,7 +20,7 @@ public class BlogApiController {
 
     private final BlogService blogService;
 
-     //HTTP 메서드가 POST일때 전달받은 URL과 동일하면 메서드로 매핑
+    //HTTP 메서드가 POST일때 전달받은 URL과 동일하면 메서드로 매핑
     @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticles(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
@@ -24,5 +28,17 @@ public class BlogApiController {
         // 요청한 자원이 성공적으로 생성 되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    // 글 조회 컨트롤러
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
