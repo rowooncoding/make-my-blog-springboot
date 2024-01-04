a5f85f2abbb68c4dedd8d73e9a139a295e4de16e
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-04 16:08:23 +0900

    feat: 해당 article에 대한 값을 반환할 컨트롤러 작성

--- src/main/java/com/rowoon/myblog/controller/BlogViewController.java
+++ src/main/java/com/rowoon/myblog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.rowoon.myblog.controller;
 
+import com.rowoon.myblog.domain.Article;
 import com.rowoon.myblog.dto.ArticleListViewResponse;
+import com.rowoon.myblog.dto.ArticleResponse;
 import com.rowoon.myblog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +25,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); // 1. 블로그 글 리스트 저장
 
         return "articleList"; // 2. articleList.html라는 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleResponse(article));
+
+        return "article";
     }
 }
