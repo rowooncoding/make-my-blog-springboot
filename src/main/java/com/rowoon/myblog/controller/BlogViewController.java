8c3edeae46ac73a32e24c9946737cfcaf16f65e1
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-04 16:41:51 +0900

    feat: 글수정작성 컨트롤러 완성

--- src/main/java/com/rowoon/myblog/controller/BlogViewController.java
+++ src/main/java/com/rowoon/myblog/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -32,5 +33,18 @@ public class BlogViewController {
         Article article = blogService.findById(id);
         model.addAttribute("article", new ArticleViewResponse(article));
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    // 1. id키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if(id == null) { //  2. id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        }else{ // 3. id가 있으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
