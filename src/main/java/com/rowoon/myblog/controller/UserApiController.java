9c7cc1e3b367759fc0c654a094d136c8d86eeb28
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-10 16:50:34 +0900

    feat: 로그아웃 매서드 생성

--- src/main/java/com/rowoon/myblog/controller/UserApiController.java
+++ src/main/java/com/rowoon/myblog/controller/UserApiController.java
@@ -2,8 +2,14 @@ package com.rowoon.myblog.controller;
 
 import com.rowoon.myblog.dto.AddUserRequest;
 import com.rowoon.myblog.service.UserService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -16,5 +22,11 @@ public class UserApiController {
     public String signup(AddUserRequest request) {
         userService.save(request); // 회원가입 메서드 호출
         return "redirect:/login"; // 회원가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
