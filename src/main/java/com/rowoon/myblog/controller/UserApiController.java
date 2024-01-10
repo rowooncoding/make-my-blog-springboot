1b77465cf6ab45eb787d565241ea90672b86e412
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-10 16:46:06 +0900

    feat: 클라이언트에서 유저 정보를 받아오는 컨트롤러 생성

---
+++ src/main/java/com/rowoon/myblog/controller/UserApiController.java
@@ -0,0 +1,20 @@
+package com.rowoon.myblog.controller;
+
+import com.rowoon.myblog.dto.AddUserRequest;
+import com.rowoon.myblog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request) {
+        userService.save(request); // 회원가입 메서드 호출
+        return "redirect:/login"; // 회원가입이 완료된 이후에 로그인 페이지로 이동
+    }
+}
