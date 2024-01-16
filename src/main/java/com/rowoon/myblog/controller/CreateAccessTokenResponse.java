467f93c4c141a4d5484a925496cd0ec83c8b713a
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-16 16:14:22 +0900

    feat : 토큰 생성 요청 및 응답 담당 dto 생성

---
+++ src/main/java/com/rowoon/myblog/controller/CreateAccessTokenResponse.java
@@ -0,0 +1,10 @@
+package com.rowoon.myblog.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateAccessTokenResponse {
+    private String accessToken;
+}
