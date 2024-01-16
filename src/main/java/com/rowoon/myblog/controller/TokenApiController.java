fff8fe292fdb29c386c61810cf61c905a2791375
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-16 16:18:08 +0900

    feat : 토큰 생성을 요청할 컨트롤러 생성

---
+++ src/main/java/com/rowoon/myblog/controller/TokenApiController.java
@@ -0,0 +1,26 @@
+package com.rowoon.myblog.controller;
+
+import com.rowoon.myblog.config.jwt.TokenProvider;
+import com.rowoon.myblog.dto.CreateAccessTokenRequest;
+import com.rowoon.myblog.dto.CreateAccessTokenResponse;
+import com.rowoon.myblog.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
