5ab3bfe761587d75c2ed841e16520db6c1f342c4
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-01-16 15:58:05 +0900

    feat : 리프레시 토큰관련 레포생성

--- src/main/java/com/rowoon/myblog/repository/RefreshTokenRepository.java
+++ src/main/java/com/rowoon/myblog/repository/RefreshTokenRepository.java
@@ -9,5 +9,4 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     Optional<RefreshToken> findByUserId(Long userId);
 
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
-
 }
